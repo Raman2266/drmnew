f1dc8f16f1f22af0a520b648e17c33514cfbec82
Raman2266 <manishkjangra2266@gmail.com>
2024-05-29 08:26:43 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,4 @@
 api_id = 28829496
 api_hash = "8094341dbe5bd50090de8b12a01d04b8"
-bot_token = "6715326658:AAHVLu_V2HJKquZG4kAPydJDP6G1lATc8lA"
+bot_token = "6783536077:AAEeBMgn4EzVP2x21ZidIJmxXcNXuG_9JL0"
 # Bot Username = "allinone"
